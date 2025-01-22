0c05f8655f63feecd6451a0c11188c2a74edaaeb
Kiara Ruiz <kiaraprz11@gmail.com>
2025-01-22 00:25:38 -0500

    Tarea1

---
+++ Semana1.py/Ejercicio4.py
@@ -0,0 +1,5 @@
+number = int(input("Ingrese un número: ")) 
+if number%2 == 0:
+    print("El numero es par")
+else:                                   #doble punto
+    print("El numero es impar")
---
+++ Semana1.py/Ejercicio5.py
@@ -0,0 +1,3 @@
+number = int(input("Ingrese un numero entero: "))
+suma = number*(number+1)/2
+print(suma)
