bac477142fdf5fc54cd394827cac37eb17ffdc32
Kiara Ruiz <kiaraprz11@gmail.com>
2025-01-28 23:22:44 -0500

    tarea2

--- Semana2/models/bus.py
+++ Semana2/models/bus.py
@@ -3,5 +3,5 @@ class Bus:
         self.id = id
         self.placa = placa
         self.ruta = None  # Ruta asignada al bus
-        self.horarios = []  # Horarios asignados al bus (ej. ["09:00-11:00"])
+        self.horarios = []  # Horarios asignados al bus
         self.conductores_asignados = []  # Lista de conductores asignados
